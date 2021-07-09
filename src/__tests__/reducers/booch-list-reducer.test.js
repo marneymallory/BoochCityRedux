0a87382c2ac1dc10f69a22b85f8b144ba8a40862
marneymallory <79782193+marneymallory@users.noreply.github.com>
2021-07-09 11:33:38 -0800

    WIP

--- src/__tests__/reducers/booch-list-reducer.test.js
+++ src/__tests__/reducers/booch-list-reducer.test.js
@@ -1,19 +1,28 @@
 import boochListReducer from "../../reducers/booch-list-reducer";
 
 describe('boochListReducer', () => {
-  let action;
-  const boochData = {
-    name: 'Blueberry Morning',
-    brand: 'Zip Kombucha',
-    price: 40.0,
-    flavor: 'Blueberry',
-    amountLeft: 110,
-    id: 1
-  };
+ const currentState = {
+   1: {
+     name: "Blueberry Morning",
+     brand: "Zip Kombucha",
+     price: 40.0,
+     flavor: "Blueberry",
+     amountLeft: 110,
+     id: 1,
+   },
+   2: {
+     name: "Ginger Zing",
+     brand: "Zip Kombucha",
+     price: 40.0,
+     flavor: "Ginger",
+     amountLeft: 80,
+     id: 2 },
+   }
+
   test("Should return default state if there is no action type passed into the reducer", () => {
     expect(boochListReducer({}, { type: null })).toEqual({});
   });
-  
+
   test('Should successfully add new booch data to masterBoochList', () =>{
     const { name, brand, price, flavor, amountLeft, id } = boochData;
     action = {
@@ -32,8 +41,21 @@ describe('boochListReducer', () => {
       price: price,
       flavor: flavor,
       amountLeft: amountLeft,
-      id: id
-      }
-    })
-  })
-});
+      id: id }
+      };
+
+      test ('Should successfully delete a booch',() => {
+        action = {
+          type: 'DELETE_BOOCH'
+          id: 1
+        };
+        expect(boochListReducer(currentState, action)).toEqual({
+            2: {  name: "Ginger Zing",
+                  brand: "Zip Kombucha",
+                  price: 40.0,
+                  flavor: "Ginger",
+                  amountLeft: 80,
+                  id: 2 }
+        });
+    });
+})
